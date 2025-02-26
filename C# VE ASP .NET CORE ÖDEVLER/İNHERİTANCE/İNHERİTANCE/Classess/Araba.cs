c9c3e9c9c21dfe1db96fe110d5bd072185fae791
Nihat Alperen Ünal <nihatalperen326@gmail.com>
2025-02-26 18:21:26 +0300

    ASP .NET CORE/3# ÖDEVLER
    Acun Medya Akademi'de Akademiq staj programı kapsamında yapmış olduğum ödevler.

---
+++ C# VE ASP .NET CORE ÖDEVLER/İNHERİTANCE/İNHERİTANCE/Classess/Araba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İNHERİTANCE.Classess
+{
+    public class Araba
+    {
+        public string marka;
+        public string vites;
+        public void arababilgi(string marka, string vites)
+        {
+            Console.WriteLine(marka + " " + vites + " " + "viteslidir.");
+        }
+    }
+}
---
+++ C# VE ASP .NET CORE ÖDEVLER/İNHERİTANCE/İNHERİTANCE/Program.cs
@@ -0,0 +1,26 @@
+﻿using İNHERİTANCE.Classess;
+
+Audi audi = new Audi();
+audi.marka = "Audi";
+audi.vites = "Otomatik";
+audi.arababilgi(audi.marka, audi.vites);
+Bmw bmw = new Bmw();
+bmw.marka = "BMW";
+bmw.vites = "Düz";
+bmw.arababilgi(bmw.marka, bmw.vites);
+Mercedes mercedes = new Mercedes();
+mercedes.marka = "Mercedes";
+mercedes.vites = "Otomatik";
+mercedes.arababilgi(mercedes.marka, mercedes.vites);
+Porsche porsche = new Porsche();
+porsche.marka = "Porsche";
+porsche.vites = "Otomatik";
+porsche.arababilgi(porsche.marka, porsche.vites);
+Togg togg = new Togg();
+togg.marka = "Togg";
+togg.vites = "Otomatik";
+togg.arababilgi(togg.marka, togg.vites);
+Toyota toyota = new Toyota();
+toyota.marka = "Toyota";
+toyota.vites = "Düz";
+toyota.arababilgi(toyota.marka, toyota.vites);
