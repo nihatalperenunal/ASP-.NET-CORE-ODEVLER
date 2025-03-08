55a423812b6b5f2cad547b43ef4fd92791bf3051
Nihat Alperen Ünal <nihatalperen326@gmail.com>
2025-03-09 01:59:55 +0300

    Sayı tahmin oyunu

---
+++ C# VE ASP .NET CORE ÖDEVLER/SAYİ TAHMİN OYUNU/ODEV1/ODEV1/ODEV1/Program.cs
@@ -0,0 +1,28 @@
+﻿Random rastgele = new Random();
+int dogruSayi = rastgele.Next(1, 101);
+int tahmin = 0;
+int tahminSayisi = 0;
+
+Console.WriteLine("1 ile 100 arasında bir sayı tuttum. Hadi tahmin etmeye çalış!");
+
+while (tahmin != dogruSayi)
+{
+    Console.Write("Tahmininizi girin: ");
+    tahmin = Convert.ToInt32(Console.ReadLine());
+    tahminSayisi++;
+
+    if (tahmin < dogruSayi)
+    {
+        Console.WriteLine("Daha büyük bir sayı deneyin.");
+    }
+    else if (tahmin > dogruSayi)
+    {
+        Console.WriteLine("Daha küçük bir sayı deneyin.");
+    }
+    else
+    {
+        Console.WriteLine("Tebrikler! Doğru tahmini yaptınız.");
+    }
+}
+
+Console.WriteLine("Tahmin sayınız: " + tahminSayisi);
