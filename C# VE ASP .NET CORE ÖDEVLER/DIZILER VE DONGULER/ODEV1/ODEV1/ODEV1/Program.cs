b741eb54d5be39112948d2c359e4bdaa22b5a81f
Nihat Alperen Ünal <nihatalperen326@gmail.com>
2025-03-09 01:37:21 +0300

    Diziler ve Donguler Odevi

---
+++ C# VE ASP .NET CORE ÖDEVLER/DIZILER VE DONGULER/ODEV1/ODEV1/ODEV1/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] sayilar = new int[5];
+int toplam = 0;
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Lütfen " + (i + 1) + ". Sayıyı girin: ");
+    sayilar[i] = Convert.ToInt32(Console.ReadLine());
+    toplam += sayilar[i];
+}
+double ortalama = (double)toplam / 5;
+Console.WriteLine("Girilen sayıların ortalaması: " + ortalama);
