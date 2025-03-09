b23ad5dac61f118a67ebab0277228d72913bae9b
Nihat Alperen Ünal <nihatalperen326@gmail.com>
2025-03-09 03:04:14 +0300

    Basit Hesap Makinesi Odevi

---
+++ C# VE ASP .NET CORE ÖDEVLER/BASİT HESAP MAKİNESİ/ODEV1/ODEV1/ODEV1/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("1. sayıyı girin: ");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("2. sayıyı girin: ");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Lütfen işleminizi seçin (+ - / *): ");
+char islem = Console.ReadKey().KeyChar;
+Console.WriteLine();
+if (islem == '+')
+{
+    Console.WriteLine("Sonuç: "+(sayi1 + sayi2));
+}
+else if (islem == '-')
+{
+    Console.WriteLine("Sonuç: " + (sayi1 - sayi2));
+}
+else if (islem == '*')
+{
+    Console.WriteLine("Sonuç: " + (sayi1 * sayi2));
+}
+else if (islem == '/')
+{
+    if(sayi2 != 0)
+    {
+        Console.WriteLine("Sonuç: " + (sayi1 / sayi2));
+    }
+    else
+    {
+        Console.WriteLine("Sayı sıfıra bölünemez.");
+    }
+}
+else
+{
+    Console.WriteLine("Lütfen geçerli bir işlem giriniz.");
+}
