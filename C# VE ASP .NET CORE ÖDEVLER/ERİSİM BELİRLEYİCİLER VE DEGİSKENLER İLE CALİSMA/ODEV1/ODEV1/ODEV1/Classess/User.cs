a9f399f18f45b66fd79061d46ca00283941b14e1
Nihat Alperen Ünal <nihatalperen326@gmail.com>
2025-03-09 03:44:47 +0300

    ERİSİM BELİRLEYİCİLER VE DEGİSKENLER İLE CALİSMA

---
+++ C# VE ASP .NET CORE ÖDEVLER/ERİSİM BELİRLEYİCİLER VE DEGİSKENLER İLE CALİSMA/ODEV1/ODEV1/ODEV1/Classess/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODEV1.Classess
+{
+    public class User
+    {
+        private string adSoyad;
+        private int yas;
+
+        public string Email { get; set; }
+
+        // Getter ve Setter metotları
+        public string GetAdSoyad()
+        {
+            return adSoyad;
+        }
+
+        public void SetAdSoyad(string deger)
+        {
+            adSoyad = deger;
+        }
+
+        public int GetYas()
+        {
+            return yas;
+        }
+        public void SetYas(int deger)
+        {
+            if (deger >= 0)
+            {
+                yas = deger;
+            }
+            else
+            {
+                Console.WriteLine("Yaş 0 veya daha büyük olmalıdır.");
+            }
+        }
+
+        public void BilgileriGoster()
+        {
+            Console.WriteLine("Ad ve Soyad: "+adSoyad);
+            Console.WriteLine("Yaş: "+yas);
+        }
+    }
+}
---
+++ C# VE ASP .NET CORE ÖDEVLER/ERİSİM BELİRLEYİCİLER VE DEGİSKENLER İLE CALİSMA/ODEV1/ODEV1/ODEV1/Program.cs
@@ -0,0 +1,12 @@
+﻿using ODEV1.Classess;
+
+User kullanici = new User();
+
+// Değerleri ayarla
+kullanici.SetAdSoyad("Alperen Ünal");
+kullanici.SetYas(21);
+kullanici.Email = "alperenunal@hotmail.com";
+
+// Bilgileri yazdır
+kullanici.BilgileriGoster();
+Console.WriteLine("E-posta: "+kullanici.Email);
