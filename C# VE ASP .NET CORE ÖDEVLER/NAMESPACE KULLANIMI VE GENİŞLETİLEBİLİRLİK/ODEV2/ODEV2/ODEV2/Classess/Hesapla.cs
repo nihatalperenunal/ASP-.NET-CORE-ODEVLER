bf237dd85a05b19dac2fe47d78af8ec524119af3
Nihat Alperen Ünal <nihatalperen326@gmail.com>
2025-03-09 04:23:50 +0300

    NAME SPACE KULLANIMI VE GENISLETILEBILIRLIK

---
+++ C# VE ASP .NET CORE ÖDEVLER/NAMESPACE KULLANIMI VE GENİŞLETİLEBİLİRLİK/ODEV2/ODEV2/ODEV2/Classess/Hesapla.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IslemNamespace
+{
+    public class Hesapla
+    {
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+    }
+}
---
+++ C# VE ASP .NET CORE ÖDEVLER/NAMESPACE KULLANIMI VE GENİŞLETİLEBİLİRLİK/ODEV2/ODEV2/ODEV2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using IslemNamespace; // Hesapla sınıfını tanımladığımız namespace'i ekliyoruz
+
+namespace TestNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hesapla hesaplama = new Hesapla();
+
+            int toplam = hesaplama.Topla(10, 20);
+            Console.WriteLine($"10 + 20 = {toplam}");
+            int carpim = hesaplama.Carp(5, 6);
+            Console.WriteLine($"5 * 6 = {carpim}");
+        }
+    }
+}
