2c0c795e4fcc2f792d8537737e6ee23dc5f7884a
Nihat Alperen Ünal <nihatalperen326@gmail.com>
2025-03-29 03:30:06 +0300

    GENERIC_LIST

---
+++ C# VE ASP .NET CORE ÖDEVLER/GENERİC LİST VE NESNE KULLANIMI/GENERİC LİST VE NESNE KULLANIMI/Classess/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GENERİC_LİST_VE_NESNE_KULLANIMI.Classess
+{
+    public class Car
+    {
+        public string Marka;
+        public string Model;
+        public double FuelConsumption;
+        public double TotalDistance;
+
+        public Car(string marka, string model, double fuelConsumption, double totalDistance)
+        {
+            Marka = marka;
+            Model = model;
+            FuelConsumption = fuelConsumption;
+            TotalDistance = totalDistance;
+        }
+        public double ToplamYakitTuketimi()
+        {
+            return (TotalDistance / 100) * FuelConsumption;
+        }
+    }
+}
---
+++ C# VE ASP .NET CORE ÖDEVLER/GENERİC LİST VE NESNE KULLANIMI/GENERİC LİST VE NESNE KULLANIMI/Program.cs
@@ -0,0 +1,13 @@
+﻿using GENERİC_LİST_VE_NESNE_KULLANIMI.Classess;
+
+List<Car> cars = new List<Car>()
+{
+    new Car("Toyota","Corolla",6.5,5000),
+    new Car("BMW","X5",9.0,5000),
+    new Car("Mercedes", "C200", 8.2, 5000)
+
+};
+foreach (var car in cars)
+{
+    Console.WriteLine($"Marka: {car.Marka}, Model: {car.Model}, 100 km'de Yaktığı Yakıt: {car.FuelConsumption} litre, Toplam Yakıt Tüketimi: {car.ToplamYakitTuketimi()} litre");
+}
